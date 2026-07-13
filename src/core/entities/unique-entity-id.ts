75e933d834cc04b1c744287efffa3ea3c3a4e0d9
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-07-13 17:43:44 -0300

    initialize project with TypeScript setup and core entity structure

---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,16 @@
+import { randomUUID } from "node:crypto"
+
+
+export class UniqueEntityId{
+  private value: string
+
+  toString(){
+    return this.value
+  }
+
+  constructor(id?: string){
+    this.value = id ?? randomUUID()
+  }
+
+
+}
---
+++ src/domain/management/enterprise/entities/deliver.ts
@@ -0,0 +1,7 @@
+export interface DeliverProps{
+  name: string
+  cpf: string
+  password: string
+  createdAt: Date
+  updatedAt: Date
+}
